799a55d25b1015385fd49114bcbf79d9bb0b3ffb
PavelAL <PavelAleksin90@gmail.com>
2023-07-16 19:49:02 +0300

    1

--- les10.py
+++ les10.py
@@ -7,19 +7,19 @@ def get_proxy():
     html = requests.get('https://free-proxy-list.net/').text
     soup = BeautifulSoup(html, 'lxml')
 
-    trs = soup.find('table', id='proxylisttable').find_all('tr')[1:11]
-
+    trs = soup.find('table', attrs={'class':'table table-striped table-bordered'}).find_all('tr')[1:11]
+    trs
     proxies = []
 
     for tr in trs:
         tds = tr.find_all('td')
-        ip = tds[0].text.strip()
-        port = tds[1].text.strip()
-        schema = 'https' if 'yes' in tds[6].text.strip() else 'http'
-        proxy = {'schema': schema, 'address': ip + ':' + port}
-        proxies.append(proxy)
+        if 'no' in tds[6].text.strip():
+            ip = tds[0].text.strip()
+            port = tds[1].text.strip()
+            proxy = {'schema': 'http', 'address': ip + ':' + port}
+            proxies.append(proxy)
 
-    return choice(proxies)
+    return proxies[2]
 
 
 def get_html(url):
